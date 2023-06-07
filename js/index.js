268d40793ba8778ff6b227d05525b7dbb7ceaa2d
Simon_Paul <simonchasnovsky@gmail.com>
2023-06-07 17:08:59 -0300

    Optimized JS loading

--- js/index.js
+++ js/index.js
@@ -1,179 +1,176 @@
-$(document).ready(function(){
-
-    let controlShow = false;
-    let menuShow = false;
-    let musicShow = false;
-    let masterShow = false;
-
-    $("audio").prop("volume", 0.2);
-
-    $(".top").click(function() {
-
-        if (controlShow == false) {
-            $(".top").addClass("top-flip");
-        }
-        
-        if (controlShow == true) {
-            $(".top").removeClass("top-flip");
-        }
-
-        if (menuShow == true || musicShow == true || masterShow == true) {
-            $(".menu-container").hide("slide", {direction: "left"}, 200);
-            menuShow = false;
-            $(".music-container").hide("slide", {direction: "right"}, 200);
-            musicShow = false;
-            $(".master-container").hide("slide", {direction: "down"}, 200);
-            masterShow = false;
-            $(".control-container").delay(200).show("slide", {direction: "up"}, 200);
-            controlShow = true;
-            $(".left").removeClass("left-flip");
-            $(".right").removeClass("right-flip");
-            $(".bot").removeClass("bot-flip");
-            return;
-        }
-        
-        if (controlShow == false) {
-            $(".control-container").show("slide", {direction: "up"}, 200);
-            controlShow = true;
-            return;
-        }
-
-        if (controlShow == true) {
-            $(".control-container").hide("slide", {direction: "up"}, 200);
-            controlShow = false;
-            return;
-        }
-        
-    });
-
-    $(".left").click(function() {
-
-        if (menuShow == false) {
-            $(".left").addClass("left-flip");
-        }
-        
-        if (menuShow == true) {
-            $(".left").removeClass("left-flip");
-        }
-
-        if (controlShow == true || musicShow == true || masterShow == true) {
-            $(".control-container").hide("slide", {direction: "up"}, 200);
-            controlShow = false;
-            $(".music-container").hide("slide", {direction: "right"}, 200);
-            musicShow = false;
-            $(".master-container").hide("slide", {direction: "down"}, 200);
-            masterShow = false;
-            $(".menu-container").delay(200).show("slide", {direction: "left"}, 200);
-            menuShow = true;
-            $(".top").removeClass("top-flip");
-            $(".right").removeClass("right-flip");
-            $(".bot").removeClass("bot-flip");
-            return;
-        }
-        
-        if (menuShow == false) {
-            $(".menu-container").show("slide", {direction: "left"}, 200);
-            menuShow = true;
-            return;
-        }
-
-        if (menuShow == true) {
-            $(".menu-container").hide("slide", {direction: "left"}, 200);
-            menuShow = false;
-            return;
-        }
-
-    });
-
-    $(".right").click(function() {
-
-        if (musicShow == false) {
-            $(".right").addClass("right-flip");
-
-        }
-        
-        if (musicShow == true) {
-            $(".right").removeClass("right-flip");
-        }
-
-        if (controlShow == true || menuShow == true || masterShow == true) {
-            $(".control-container").hide("slide", {direction: "up"}, 200);
-            controlShow = false;
-            $(".menu-container").hide("slide", {direction: "left"}, 200);
-            menuShow = false;
-            $(".master-container").hide("slide", {direction: "down"}, 200);
-            masterShow = false;
-            $(".music-container").delay(200).show("slide", {direction: "right"}, 200);
-            musicShow = true;
-            $(".left").removeClass("left-flip");
-            $(".top").removeClass("top-flip");
-            $(".bot").removeClass("bot-flip");
-            return;
-        }
-        
-        if (musicShow == false) {
-            $(".music-container").show("slide", {direction: "right"}, 200);
-            musicShow = true;
-            return;
-        }
-
-        if (musicShow == true) {
-            $(".music-container").hide("slide", {direction: "right"}, 200);
-            musicShow = false;
-            return;
-        }
-
-    });
-
-    $(".bot").click(function() {
-
-        if (masterShow == false) {
-            $(".bot").addClass("bot-flip");
-        }
-        
-        if (masterShow == true) {
-            $(".bot").removeClass("bot-flip");
-        }
-
-        if (menuShow == true || musicShow == true || controlShow == true) {
-            $(".menu-container").hide("slide", {direction: "left"}, 200);
-            menuShow = false;
-            $(".music-container").hide("slide", {direction: "right"}, 200);
-            musicShow = false;
-            $(".control-container").hide("slide", {direction: "up"}, 200);
-            controlShow = false;
-            $(".master-container").delay(200).show("slide", {direction: "down"}, 200);
-            masterShow = true;
-            $(".left").removeClass("left-flip");
-            $(".right").removeClass("right-flip");
-            $(".top").removeClass("top-flip");
-            return;
-        }
-        
-        if (masterShow == false) {
-            $(".master-container").show("slide", {direction: "down"}, 200);
-            masterShow = true;
-            return;
-        }
-
-        if (masterShow == true) {
-            $(".master-container").hide("slide", {direction: "down"}, 200);
-            masterShow = false;
-            return;
-        }
-
-    });
-
-    $("audio").attr("src", "");
-    
-});
+let controlShow = false;
+let menuShow = false;
+let musicShow = false;
+let masterShow = false;
 
 let spotifyPlay = false;
 let youtubePlay = false;
+
 let youtubeShow = false;
 let spotifyShow = false;
 
 const masterIcon = document.getElementById("master-icon");
+
+$("audio").prop("volume", 0.2);
+
+$("audio").attr("src", "");
+
+$(".top").click(function() {
+
+    if (controlShow == false) {
+        $(".top").addClass("top-flip");
+    }
+    
+    if (controlShow == true) {
+        $(".top").removeClass("top-flip");
+    }
+
+    if (menuShow == true || musicShow == true || masterShow == true) {
+        $(".menu-container").hide("slide", {direction: "left"}, 200);
+        menuShow = false;
+        $(".music-container").hide("slide", {direction: "right"}, 200);
+        musicShow = false;
+        $(".master-container").hide("slide", {direction: "down"}, 200);
+        masterShow = false;
+        $(".control-container").delay(200).show("slide", {direction: "up"}, 200);
+        controlShow = true;
+        $(".left").removeClass("left-flip");
+        $(".right").removeClass("right-flip");
+        $(".bot").removeClass("bot-flip");
+        return;
+    }
+    
+    if (controlShow == false) {
+        $(".control-container").show("slide", {direction: "up"}, 200);
+        controlShow = true;
+        return;
+    }
+
+    if (controlShow == true) {
+        $(".control-container").hide("slide", {direction: "up"}, 200);
+        controlShow = false;
+        return;
+    }
+    
+});
+
+$(".left").click(function() {
+
+    if (menuShow == false) {
+        $(".left").addClass("left-flip");
+    }
+    
+    if (menuShow == true) {
+        $(".left").removeClass("left-flip");
+    }
+
+    if (controlShow == true || musicShow == true || masterShow == true) {
+        $(".control-container").hide("slide", {direction: "up"}, 200);
+        controlShow = false;
+        $(".music-container").hide("slide", {direction: "right"}, 200);
+        musicShow = false;
+        $(".master-container").hide("slide", {direction: "down"}, 200);
+        masterShow = false;
+        $(".menu-container").delay(200).show("slide", {direction: "left"}, 200);
+        menuShow = true;
+        $(".top").removeClass("top-flip");
+        $(".right").removeClass("right-flip");
+        $(".bot").removeClass("bot-flip");
+        return;
+    }
+    
+    if (menuShow == false) {
+        $(".menu-container").show("slide", {direction: "left"}, 200);
+        menuShow = true;
+        return;
+    }
+
+    if (menuShow == true) {
+        $(".menu-container").hide("slide", {direction: "left"}, 200);
+        menuShow = false;
+        return;
+    }
+
+});
+
+$(".right").click(function() {
+
+    if (musicShow == false) {
+        $(".right").addClass("right-flip");
+
+    }
+    
+    if (musicShow == true) {
+        $(".right").removeClass("right-flip");
+    }
+
+    if (controlShow == true || menuShow == true || masterShow == true) {
+        $(".control-container").hide("slide", {direction: "up"}, 200);
+        controlShow = false;
+        $(".menu-container").hide("slide", {direction: "left"}, 200);
+        menuShow = false;
+        $(".master-container").hide("slide", {direction: "down"}, 200);
+        masterShow = false;
+        $(".music-container").delay(200).show("slide", {direction: "right"}, 200);
+        musicShow = true;
+        $(".left").removeClass("left-flip");
+        $(".top").removeClass("top-flip");
+        $(".bot").removeClass("bot-flip");
+        return;
+    }
+    
+    if (musicShow == false) {
+        $(".music-container").show("slide", {direction: "right"}, 200);
+        musicShow = true;
+        return;
+    }
+
+    if (musicShow == true) {
+        $(".music-container").hide("slide", {direction: "right"}, 200);
+        musicShow = false;
+        return;
+    }
+
+});
+
+$(".bot").click(function() {
+
+    if (masterShow == false) {
+        $(".bot").addClass("bot-flip");
+    }
+    
+    if (masterShow == true) {
+        $(".bot").removeClass("bot-flip");
+    }
+
+    if (menuShow == true || musicShow == true || controlShow == true) {
+        $(".menu-container").hide("slide", {direction: "left"}, 200);
+        menuShow = false;
+        $(".music-container").hide("slide", {direction: "right"}, 200);
+        musicShow = false;
+        $(".control-container").hide("slide", {direction: "up"}, 200);
+        controlShow = false;
+        $(".master-container").delay(200).show("slide", {direction: "down"}, 200);
+        masterShow = true;
+        $(".left").removeClass("left-flip");
+        $(".right").removeClass("right-flip");
+        $(".top").removeClass("top-flip");
+        return;
+    }
+    
+    if (masterShow == false) {
+        $(".master-container").show("slide", {direction: "down"}, 200);
+        masterShow = true;
+        return;
+    }
+
+    if (masterShow == true) {
+        $(".master-container").hide("slide", {direction: "down"}, 200);
+        masterShow = false;
+        return;
+    }
+
+});
 
 function onOff(audioId, btnId, displaySpanClass, onOffClass) {
     const audio = document.getElementById(audioId);
