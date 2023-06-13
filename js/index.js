1f866098a366199c7a03adab02ad26c860d56a95
Simon_Paul <simonchasnovsky@gmail.com>
2023-06-13 15:47:30 -0300

    Migrated away from PHP into full JS

--- js/index.js
+++ js/index.js
@@ -9,6 +9,170 @@ let youtubePlay = false;
 let youtubeShow = false;
 let spotifyShow = false;
 
+const defaultSounds = [
+    "Light rain on ceiling",
+    "Moderate bursty wind",
+    "Light thunderstorm",
+    "Busy coffee shop",
+    "Moderate traffic",
+    "Chill car ride",
+    "Forest afternoon",
+    "Campfire",
+    "Beachfront"
+];
+
+const defaultIcons = [
+    "fa-solid fa-cloud-rain",
+    "fa-solid fa-wind",
+    "fa-solid fa-cloud-bolt",
+    "fa-solid fa-mug-hot",
+    "fa-solid fa-traffic-light",
+    "fa-solid fa-car",
+    "fa-solid fa-tree",
+    "fa-solid fa-fire",
+    "fa-solid fa-umbrella-beach"
+];
+
+const allSounds = [
+    "Beachfront",
+    "Busy coffee shop",
+    "Busy restaurant",
+    "Campfire",
+    "Chill car ride",
+    "Chill forest",
+    "Forest afternoon",
+    "Forest night",
+    "Forest tranquility",
+    "Grassland",
+    "Heavy bursty wind",
+    "Light rain on ceiling",
+    "Light thunderstorm",
+    "Light water flow",
+    "Moderate bursty wind",
+    "Moderate constant wind",
+    "Moderate rain",
+    "Moderate river current",
+    "Moderate traffic",
+    "Quick car ride",
+    "Restaurant",
+    "River flow",
+    "Shower on grass",
+    "Strong waterfall"
+];
+
+const icons = {
+    "Beachfront":"fa-solid fa-umbrella-beach",
+    "Busy coffee shop":"fa-solid fa-mug-hot",
+    "Busy restaurant":"fa-solid fa-utensils",
+    "Campfire":"fa-solid fa-fire",
+    "Chill car ride":"fa-solid fa-car",
+    "Chill forest":"fa-solid fa-tree",
+    "Forest afternoon":"fa-solid fa-tree",
+    "Forest night":"fa-solid fa-tree",
+    "Forest tranquility":"fa-solid fa-tree",
+    "Grassland":"fa-solid fa-tree",
+    "Heavy bursty wind":"fa-solid fa-wind",
+    "Light rain on ceiling":"fa-solid fa-cloud-rain",
+    "Light thunderstorm":"fa-solid fa-cloud-bolt",
+    "Light water flow":"fa-solid fa-water",
+    "Moderate bursty wind":"fa-solid fa-wind",
+    "Moderate constant wind":"fa-solid fa-wind",
+    "Moderate rain":"fa-solid fa-cloud-rain",
+    "Moderate river current":"fa-solid fa-water",
+    "Moderate traffic":"fa-solid fa-traffic-light",
+    "Quick car ride":"fa-solid fa-car",
+    "Restaurant":"fa-solid fa-utensils",
+    "River flow":"fa-solid fa-water",
+    "Shower on grass":"fa-solid fa-cloud-rain",
+    "Strong waterfall":"fa-solid fa-droplet"
+};
+
+for (let i = 0; i < 9; i++) {
+    let controlDiv = document.createElement('div');
+    controlDiv.setAttribute("class", "control" + i);
+
+    controlDiv.innerHTML =  `
+        <div class="switcher-holder switcher-h${+ i}">
+            <div class="switcher-container">
+                <span>Audio Switcher</span>
+                <div class="audio-switcher">
+                    <ul>
+                    ${allSounds.map(sound => `<li class="switcher-element ${sound}" 
+                    onclick="switchAudio(
+                        '${sound}', 
+                        'audio${+ i}', 
+                        '${sound}', 
+                        'displaySpan${+ i}', 
+                        'displayIcon${+ i}', 
+                        '${icons[sound]}', 
+                        'btn-icon${+ i}' 
+                        )">
+                        ${sound}
+                        <i class="${icons[sound]}"></i>
+                    </li>`).join('')}
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <audio 
+            id="audio${+ i}" 
+            class="player${+ i}" 
+            src="audio/${defaultSounds[i]}.opus" 
+            loop>
+        </audio>
+        <div 
+            onclick="toggleSwitcher(
+                'switcher-h${+ i}', 
+                'sw-arrow${+ i}'
+                )" 
+            class="switcher-arrow sw-arro${+ i}">
+        </div>
+        <div class="audio-display">
+            <span class="displaySpan${+ i}">${defaultSounds[i]}</span>
+            <i id="displayIcon${i}" 
+            class="${defaultIcons[i]}"></i>
+        </div>
+        <div class="play-pause-container">
+            <button 
+                onclick="pausePlay(
+                    'audio${+ i}', 
+                    'btn-icon${+ i}'
+                    )" 
+                type="button" 
+                class="int-btn pause-play${+ i}"> 
+                <i id="btn-icon${+ i}" 
+                class="pnp-icon fa-solid fa-play btn-inactive"></i>
+            </button> 
+        </div>
+        <div class="on-off-container">
+            <button 
+                onclick="onOff(
+                    'audio${+ i}', 
+                    'btn-icon${+ i}',
+                    'displaySpan${+ i}',
+                    'on-off${+ i}'
+                    )" 
+                type="button" 
+                class="on-off-btn btn-inactive on-off${+ i}">
+                <i class="fa-solid fa-power-off"></i>
+            </button>
+        </div>
+        <div class="volume-container">
+            <input 
+                oninput="changeVolume(
+                    'audio${+ i}', 
+                    this.value
+                )" 
+                class="volume${+ i} slider" 
+                id="volume${+ i}" 
+                type="range" 
+                value="20">
+        </div>
+    `;
+    
+    document.getElementById("control-grid").appendChild(controlDiv);
+}
+
 const masterIcon = document.getElementById("master-icon");
 
 $("audio").prop("volume", 0.2);
@@ -273,7 +437,7 @@ function masterPausePlay() {
         if (youtubePlay == false && youtubeShow == true) {
             playYoutube();
         }
-    } else if (audios.length != 0){
+    } else if (audios.length != 0) {
 
         audios.forEach(element => {
             element.pause()
@@ -300,27 +464,27 @@ function changeVolume(audioId, slider) {
 }
 
 function stopYoutube() {
-        const youtubeEmbedWindow = document.querySelector('iframe[src*="youtube.com/"]').contentWindow;
-        youtubeEmbedWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
-        youtubePlay = false;
+    const youtubeEmbedWindow = document.querySelector('iframe[src*="youtube.com/"]').contentWindow;
+    youtubeEmbedWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+    youtubePlay = false;
 }
 
 function playYoutube() {
-        const youtubeEmbedWindow = document.querySelector('iframe[src*="youtube.com/"]').contentWindow;
-        youtubeEmbedWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
-        youtubePlay = true;
+    const youtubeEmbedWindow = document.querySelector('iframe[src*="youtube.com/"]').contentWindow;
+    youtubeEmbedWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+    youtubePlay = true;
 }
 
 function stopSpotify() {
-        const spotifyEmbedWindow = document.querySelector('iframe[src*="spotify.com/embed"]').contentWindow;
-        spotifyEmbedWindow.postMessage({command: 'pause'}, '*');
-        spotifyPlay = false;
+    const spotifyEmbedWindow = document.querySelector('iframe[src*="spotify.com/embed"]').contentWindow;
+    spotifyEmbedWindow.postMessage({command: 'pause'}, '*');
+    spotifyPlay = false;
 }
 
 function playSpotify() {
-        const spotifyEmbedWindow = document.querySelector('iframe[src*="spotify.com/embed"]').contentWindow;
-        spotifyEmbedWindow.postMessage({command: 'toggle'}, '*');
-        spotifyPlay = true;
+    const spotifyEmbedWindow = document.querySelector('iframe[src*="spotify.com/embed"]').contentWindow;
+    spotifyEmbedWindow.postMessage({command: 'toggle'}, '*');
+    spotifyPlay = true;
 }
 
 function embedMusic() {
@@ -383,7 +547,7 @@ function toggleSwitcher(switcherClass, arrowClass) {
 }
 
 function switchAudio(audioFile, audioElement, switcherElement, 
-    displaySpan, displayIcon, iconClass, btnIconId) {
+    displaySpan, displayIcon, iconClass) {
     const audio = document.getElementById(audioElement);
     const switcher = document.getElementsByClassName(switcherElement);
     const span = document.getElementsByClassName(displaySpan);
@@ -425,3 +589,4 @@ function switchAudio(audioFile, audioElement, switcherElement,
         }
     }   
 }
+
